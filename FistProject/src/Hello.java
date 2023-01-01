8a868e1f2f2c5e577c77c6f85f4303a9774bb54e
Do hieu Kid2D <hieukyluat@gmail.com>
2023-01-01 10:34:57 +0700

    fist load

---
+++ FistProject/src/Hello.java
@@ -0,0 +1,16 @@
+import java.math.MathContext;
+import java.util.Scanner;
+
+public class Hello {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+         int day;
+         System.out.println("Nhap vap so nguyen 2->8: ");
+         day= sc.nextInt();
+         
+         
+         
+        
+	}
+}
---
+++ day_of_month/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Dkid_22
+ *
+ */
+module day_of_month {
+}
